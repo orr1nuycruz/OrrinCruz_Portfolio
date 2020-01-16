90d50255569a7f77ea51edd8b6c29ff91807162e
orr1nuycruz <orr1n@hotmail.com>
2020-01-16 01:37:53 -0500

    Added update, delete function for CRUD

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -8,6 +8,19 @@
     function Start(){
         console.log(`%c App Started...`, 
         "font-size: 20px; color: blue; font-weight: bold");
+
+        $(".btn-danger").click(function(event){
+            if(!confirm("Are you sure?")) {
+                event.preventDefault();
+                window.location.assign("/contact-list");
+            }
+        });
+
+        /*
+        document.querySelectorAll(".btn-danger").addEventListener("click", () =>{
+
+        }); 
+        */
     }
 
     window.addEventListener("load", Start);
--- server/routes/contact.js
+++ server/routes/contact.js
@@ -51,7 +51,64 @@ router.post('/add', (req, res, next) =>{
     });
 });
 
+/* GET request for displaying the Edit Page */
 
+router.get('/edit/:id', (req, res, next) =>{
+    let id = req.params.id;
+
+    contactModel.findById(id, (err, contactObject) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            // show the edit view
+            res.render('contacts/edit',{
+                title: 'Edit Contact',
+                contact: contactObject
+            });
+        }
+    });
+});
+
+/* POST request - update the database from the database */
+router.post('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    let updatedContact = contactModel({
+        "_id": id,
+        "firstName" : req.body.firstName,
+        "lastName" : req.body.lastName,
+        "age" : req.body.age
+    });
+
+    contactModel.update({_id: id}, updatedContact, (err) =>{
+        if(err) {
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            // refresh the contact list
+            res.redirect('/contact-list');
+        }
+    });
+});
+
+/* GET request to perform the delete action */
+router.get('/delete/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    contactModel.remove({_id: id}, (err) => {
+        if (err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            // refresh the contact list
+            res.redirect('/contact-list');
+        }
+    });
+});
 
 
 
