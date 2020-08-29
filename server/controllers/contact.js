6aa409dbf8412fa95adb9b8b2a0972fe20e9e5f2
orr1nuycruz <orr1n@hotmail.com>
2020-08-28 22:45:28 -0400

    switched to local mongo and added aunthentication to contact list

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -4,62 +4,70 @@ let router = express.Router();
 // create a reference to the db schema
 let contactModel = require('../models/contact');
 
-module.exports.displayContactList = (req, res, next) =>{
-    contactModel.find((err, contactList)=>{
-        if(err){
+module.exports.displayContactList = (req, res, next) => {
+    contactModel.find((err, contactList) => {
+        if (err) {
             return console.error(err);
         }
-        else{
-            //console.log(contactList);
-            res.render('contacts/index', {
-                title: "Contact List",
-                contactList: contactList
-            });
+        else {
+            if (!req.user) {
+                res.redirect('/login');
+            }
+            else {
+                //console.log(contactList);
+                res.render('contacts/index', {
+                    title: "Contact List",
+                    contactList: contactList,
+                    displayName: req.user ? req.user.displayName : ''
+                });
+            }
         }
     });
 }
 
-module.exports.displayAddPage = (req, res, next)=> {
-    res.render('contacts/add',{
-        title: 'Add New Person'
+module.exports.displayAddPage = (req, res, next) => {
+    res.render('contacts/add', {
+        title: 'Add New Person',
+        displayName: req.user ? req.user.displayName : ''
     });
 }
 
-module.exports.processAddPage = (req, res, next) =>{
+module.exports.processAddPage = (req, res, next) => {
 
     console.log(req.body.firstName);
 
     let newContact = contactModel({
-        "firstName" : req.body.firstName,
-        "lastName" : req.body.lastName,
-        "age" : req.body.age
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
     });
 
-    contactModel.create(newContact, (err, contactModel)=> {
-        if(err){
+    contactModel.create(newContact, (err, contactModel) => {
+        if (err) {
             console.log(err);
             res.end(err);
         }
-        else{
-        /* refresh the contact list */
+        else {
+            /* refresh the contact list */
             res.redirect('/contact-list');
         }
     });
 }
 
-module.exports.displayEditPage = (req, res, next) =>{
+module.exports.displayEditPage = (req, res, next) => {
     let id = req.params.id;
 
     contactModel.findById(id, (err, contactObject) => {
-        if(err){
+        if (err) {
             console.log(err);
             res.end(err);
         }
-        else{
+        else {
             // show the edit view
-            res.render('contacts/edit',{
+            res.render('contacts/edit', {
                 title: 'Edit Contact',
-                contact: contactObject
+                contact: contactObject,
+                displayName: req.user ? req.user.displayName : ''
             });
         }
     });
@@ -70,17 +78,17 @@ module.exports.processEditPage = (req, res, next) => {
 
     let updatedContact = contactModel({
         "_id": id,
-        "firstName" : req.body.firstName,
-        "lastName" : req.body.lastName,
-        "age" : req.body.age
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
     });
 
-    contactModel.update({_id: id}, updatedContact, (err) =>{
-        if(err) {
+    contactModel.update({ _id: id }, updatedContact, (err) => {
+        if (err) {
             console.log(err);
             res.end(err);
         }
-        else{
+        else {
             // refresh the contact list
             res.redirect('/contact-list');
         }
@@ -90,12 +98,12 @@ module.exports.processEditPage = (req, res, next) => {
 module.exports.processDelete = (req, res, next) => {
     let id = req.params.id;
 
-    contactModel.remove({_id: id}, (err) => {
-        if (err){
+    contactModel.remove({ _id: id }, (err) => {
+        if (err) {
             console.log(err);
             res.end(err);
         }
-        else{
+        else {
             // refresh the contact list
             res.redirect('/contact-list');
         }
